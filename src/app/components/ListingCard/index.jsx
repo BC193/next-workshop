982cffea66a65e409c019e91cbc818fa6bde74ac
Brandon Chung <BJHC051304@gmail.com>
2024-04-06 15:09:34 -0700

    completed exercises

--- src/app/components/ListingCard/index.jsx
+++ src/app/components/ListingCard/index.jsx
@@ -1,9 +1,12 @@
 import styles from "./style.module.css";
 
-const ListingCard = () => {
+const ListingCard = ({image, name, price}) => {
   return (
     <div className={styles.card}>
-      
+      <img className={styles.card_img}  src = {image}/>
+      <h1 className={styles.card_title}> {name} </h1>
+      <p className={styles.card_price}> {price} </p>
+      <button className={styles.card_button}> Add to Cart </button>
     </div>
   );
 };
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,23 @@
 import storeInfo from "./storeInfo";
 import styles from "./page.module.css";
+import ListingCard from "./components/ListingCard";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1 className={styles.store_header}>Pirate Marketplace</h1>
+      <h1 className={styles.store_header}>Pirate Marketplace!</h1>
+      <div className={styles.store_cards}>
+      <ListingCard 
+        image = {'./assets/item2.jpg'}
+        name = {'Parrot'}
+        price = {'$110'}
+      />
+      <ListingCard 
+        image = {'./assets/item2.jpg'}
+        name = {'Parrot'}
+        price = {'$110'}
+      />
+      </div>
     </main>
   );
 }
